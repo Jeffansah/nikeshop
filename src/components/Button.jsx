445dfb388b3946b2c56911e6846d906dd08b09c5
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-08-21 04:14:16 +0000

    Hero Text Added

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ label, icon }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-urbanist leading-none text-lg bg-coral-red rounded-full text-white border-coral-red">
+      {label} <img src={icon} alt="" className="ml-2 rounded-full w-5 h-5 " />
+    </button>
+  );
+};
+
+export default Button;
