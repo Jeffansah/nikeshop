88ce31f0c8b1c39a26cc3db2873a9f359ffc9056
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-08-31 15:38:41 +0000

    Super Quality Section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 const Button = ({ label, icon }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-urbanist leading-none text-lg bg-coral-red rounded-full text-white border-coral-red">
-      {label} <img src={icon} alt="" className="ml-2 rounded-full w-5 h-5 " />
+      {label}{" "}
+      {icon && <img src={icon} alt="" className="ml-2 rounded-full w-5 h-5 " />}
     </button>
   );
 };
