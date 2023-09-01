cb84101dea1c2c19de92315a44d90d561711b2fc
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-01 00:07:32 +0000

    Subscribe section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,18 @@
-const Button = ({ label, icon, backgroundColor, textColor, borderColor }) => {
+const Button = ({
+  label,
+  icon,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-urbanist leading-none ${
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red border-coral-red text-white"
-      } text-lg  rounded-full`}
+      } text-lg  rounded-full ${fullWidth && "w-full"}`}
     >
       {label}{" "}
       {icon && <img src={icon} alt="" className="ml-2 rounded-full w-5 h-5 " />}
