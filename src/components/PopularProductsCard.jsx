26c40af5d38b0c8e809966907920c9b17376c795
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-09-01 01:24:14 +0000

    Update PopularProductsCard.jsx

--- src/components/PopularProductsCard.jsx
+++ src/components/PopularProductsCard.jsx
@@ -3,7 +3,7 @@ import { star } from "../assets/icons";
 
 export const PopularProductsCard = ({ imgURL, name, price, rating }) => {
   return (
-    <div className="flex flex-1 flex-col w-full ">
+    <div className="flex flex-1 flex-col w-full cursor-pointer">
       <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
